3cbf700c5b2d6f7feeae7736593002f11715ef44
Subhankar Pal <developer.subho57@gmail.com>
2021-11-01 00:59:54 +0530

    3rd row added

--- application/resources/views/pages/home/admin/widgets/third-row/wrapper.blade.php
+++ application/resources/views/pages/home/admin/widgets/third-row/wrapper.blade.php
@@ -6,5 +6,15 @@
     <!--PROJECTS-->
     @include('pages.home.admin.widgets.third-row.projects') --}}
 
+    <!--PROJECTS PENDING-->
+    @include('pages.home.team.widgets.first-row.projects-pending')
 
+    <!--PROJECTS COMPLETED-->
+    @include('pages.home.team.widgets.first-row.tasks-new')
+
+    <!--INVOICES DUE-->
+    @include('pages.home.team.widgets.first-row.tasks-inprogress')
+
+    <!--INVOICES OVERDUE-->
+    @include('pages.home.team.widgets.first-row.tasks-feedback')
 </div>
