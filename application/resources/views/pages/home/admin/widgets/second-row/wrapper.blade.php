b287b2fbd72c5dedc04e84171b3d1e167763cde0
Subhankar Pal <developer.subho57@gmail.com>
2021-11-01 00:15:10 +0530

    disabled income vs expense graph

--- application/resources/views/pages/home/admin/widgets/second-row/wrapper.blade.php
+++ application/resources/views/pages/home/admin/widgets/second-row/wrapper.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 
     <!--INCOME-->
-    @include('pages.home.admin.widgets.second-row.income')
+    {{-- @include('pages.home.admin.widgets.second-row.income') --}}
 
 
     <!--LEADS-->
