6333942bbc1d24c69a5437a22242d1ff1d40c1c9
Subhankar Pal <developer.subho57@gmail.com>
2021-11-01 01:51:30 +0530

    reverted bhaskars changes

--- application/resources/views/pages/items/components/modals/add-edit-inc.blade.php
+++ application/resources/views/pages/items/components/modals/add-edit-inc.blade.php
@@ -22,24 +22,13 @@
             </div>
         </div>
         <!--units-->
-        <!-- height -->
         <div class="form-group row">
-            <label class="col-sm-12 col-lg-3 text-left control-label col-form-label  required">{{ cleanLang(__('lang.units_height')) }}*
+            <label class="col-sm-12 col-lg-3 text-left control-label col-form-label  required">{{ cleanLang(__('lang.units')) }}*
                 <span class="align-middle text-info font-16" data-toggle="tooltip" title="{{ cleanLang(__('lang.units_examples')) }}"
                     data-placement="top"><i class="ti-info-alt"></i></span></label>
             <div class="col-sm-12 col-lg-9">
-                <input type="text" class="form-control form-control-sm" id="item_unit" name="item_unit_height"
-                    value="{{ $item->item_unit_height ?? '' }}">
-            </div>
-        </div>
-        <!-- width -->
-        <div class="form-group row">
-            <label class="col-sm-12 col-lg-3 text-left control-label col-form-label  required">{{ cleanLang(__('lang.units_width')) }}*
-                <span class="align-middle text-info font-16" data-toggle="tooltip" title="{{ cleanLang(__('lang.units_examples')) }}"
-                    data-placement="top"><i class="ti-info-alt"></i></span></label>
-            <div class="col-sm-12 col-lg-9">
-                <input type="text" class="form-control form-control-sm" id="item_unit" name="item_unit_width"
-                    value="{{ $item->item_unit_width ?? '' }}">
+                <input type="text" class="form-control form-control-sm" id="item_unit" name="item_unit"
+                    value="{{ $item->item_unit ?? '' }}">
             </div>
         </div>
 
