0ea585c67693a075d11df21bd78ab1cf637b2a08
myusername <sajal1943@gmail.com>
2021-11-01 21:31:44 +0530

    changees

--- application/resources/views/pages/bill/components/elements/line-plain.blade.php
+++ application/resources/views/pages/bill/components/elements/line-plain.blade.php
@@ -22,7 +22,7 @@
     <td class="form-group x-unit bill_col_unit">
         <!--height-->
         <div class="input-group input-group-sm m-b-4">
-            <span class="input-group-addon mb-2 pb-4" id="fx-line-item-hrs" >{{ cleanLang(__('lang.units_height')) }}</span>
+            <span class="input-group-addon mb-5 pb-5" id="fx-line-item-hrs" >{{ cleanLang(__('lang.units_height')) }}</span>
             <input type="number" class="form-control js_item_unit_height calculation-element js_line_validation_item" name="js_item_unit_height[{{ $lineitem->lineitem_id ?? '' }}]" value="{{ $lineitem->lineitem_unit_height ?? '' }}">
         </div>
         <!--width-->
