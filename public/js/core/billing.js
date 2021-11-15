17f2a97dbbc2b57942539f0525dbd1400c77a237
myusername <sajal1943@gmail.com>
2021-11-15 19:42:09 +0530

    #18

--- public/js/core/billing.js
+++ public/js/core/billing.js
@@ -260,14 +260,14 @@ NXINVOICE.DOM.itemNewLine = function (data = {}) {
         // // lineitem.find(".item_unit_height").css({"display":"none"});
 
         // lineitem.find(".bill_col_unit").css({"display":"none"});
-        $(".bill_col_unit").hide();
-        // $(".item_unit_height").hide();
-        // $(".item_unit_width").hide();
-         $(".bill_col_unit").css({"display":"none"});
-        // $(".item_unit_height").css({"display":"none"}); 
-       //
-        $("#unit_toggle_output").hide();
-        $("#unit_toggle_output").css({"display":"none"});
+        // $(".bill_col_unit").hide();
+    //     // $(".item_unit_height").hide();
+    //     // $(".item_unit_width").hide();
+    //      $(".bill_col_unit").css({"display":"none"});
+    //     // $(".item_unit_height").css({"display":"none"}); 
+    //    //
+    //     $("#unit_toggle_output").hide();
+    //     $("#unit_toggle_output").css({"display":"none"});
         // lineitem.find(".item_unit_width").css({"display":"none"});
     }
 
@@ -304,9 +304,9 @@ NXINVOICE.DOM.itemNewLine = function (data = {}) {
     self.blur();
 };
 
-$(".bill_col_unit").css({"display":"none"});
-$("#unit_toggle_output").css({"display":"none"});
-$(".bill_col_unit").hide();
+// $(".bill_col_unit").css({"display":"none"});
+// $("#unit_toggle_output").css({"display":"none"});
+// $(".bill_col_unit").hide();
 
 /*----------------------------------------------------------------
  * [LINE ITEM]
