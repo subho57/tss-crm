d4b1dc6b5df91bed2180a940828a201be9e9c3d6
bhaskar sengupta <53405610+kudos-dot-com@users.noreply.github.com>
2021-11-15 19:19:46 +0530

    Update billing.js

--- public/js/core/billing.js
+++ public/js/core/billing.js
@@ -263,11 +263,11 @@ NXINVOICE.DOM.itemNewLine = function (data = {}) {
         // $(".bill_col_unit").hide();
         // $(".item_unit_height").hide();
         // $(".item_unit_width").hide();
-        // $(".bill_col_unit").css({"display":"none"});
+         $(".bill_col_unit").css({"display":"none"});
         // $(".item_unit_height").css({"display":"none"}); 
        //
-        // $("#unit_toggle_output").hide();
-        // $("#unit_toggle_output").css({"display":"none"});
+        $("#unit_toggle_output").hide();
+        $("#unit_toggle_output").css({"display":"none"});
         // lineitem.find(".item_unit_width").css({"display":"none"});
     }
 
