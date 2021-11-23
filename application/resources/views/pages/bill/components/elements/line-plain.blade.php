2a0982e1f197107677e29be01d8381cf8e274668
Subhankar Pal <developer.subho57@gmail.com>
2021-11-24 01:34:14 +0530

    bug fix

--- application/resources/views/pages/bill/components/elements/line-plain.blade.php
+++ application/resources/views/pages/bill/components/elements/line-plain.blade.php
@@ -30,12 +30,13 @@
         <!--height-->
         <div class="input-group input-group-sm item_unit_height">
             <span class="input-group-addon item_unit_height mb-1 pb-5" id="fx-line-item-hrs" >{{ cleanLang(__('lang.units_height')) }}<small><br>(in mm)</small><br/></span>
-            <input type="number" class="form-control js_item_unit_height calculation-element js_line_validation_item mb-1 pb-5" name="js_item_unit_height[{{ $lineitem->lineitem_id ?? '' }}]">
+            <input type="number" class="form-control js_item_unit_height calculation-element js_line_validation_item mb-1 pb-5" name="js_item_unit_height[{{ $lineitem->lineitem_id ?? '' }}]"
+             value="{{  explode('x', $lineitem->lineitem_unit ?? '0x0')[0] }}">
         </div>
         <!--width-->
         <div class="input-group input-group-sm item_unit_width">
             <span class="input-group-addon item_unit_width pb-5" id="fx-line-item-min">{{ cleanLang(__('lang.units_width')) }}<small><br>(in mm)</small><br/></span>
-            <input type="number" class="form-control js_item_unit_width calculation-element js_line_validation_item pb-5" name="js_item_unit_width[{{ $lineitem->lineitem_id ?? '' }}]">
+            <input type="number" class="form-control js_item_unit_width calculation-element js_line_validation_item pb-5" name="js_item_unit_width[{{ $lineitem->lineitem_id ?? '' }}]" value="{{ explode('x', $lineitem->lineitem_unit ?? '0x0')[1] }}">
         </div>
         {{-- <input class="form-control form-control-sm js_item_unit js_line_validation_item" type="text"
             name="js_item_unit[{{ $lineitem->lineitem_id ?? '' }}]" value="{{ $lineitem->lineitem_unit ?? '' }}"> --}}
