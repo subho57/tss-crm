1d78091aca0c43da5bd3ab838618a9d4223d28b6
Subhankar Pal <developer.subho57@gmail.com>
2021-11-27 04:00:04 +0530

    bug fix

--- application/resources/views/pages/items/components/table/ajax.blade.php
+++ application/resources/views/pages/items/components/table/ajax.blade.php
@@ -22,7 +22,9 @@
         @endif
     </td>
     <td class="items_col_description">
+        @if(strrpos($item->item_description ?? ' ( ) ', ')') != false)
         {{ explode(')', explode('(', $item->item_description ?? ' ( ) ')[1])[0] }}
+        @endif
     </td>
     <td class="items_col_rate" id="items_col_rate_{{ $item->item_id }}">
         {{ runtimeMoneyFormat($item->item_rate) }} 
