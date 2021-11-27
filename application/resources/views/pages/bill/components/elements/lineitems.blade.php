b917dc76f4d488d04d42cc311806fee6b9e251ae
myusername <sajal1943@gmail.com>
2021-11-28 00:51:28 +0530

    #42

--- application/resources/views/pages/bill/components/elements/lineitems.blade.php
+++ application/resources/views/pages/bill/components/elements/lineitems.blade.php
@@ -2,8 +2,9 @@
 <tr>
     <!--description-->
     <td class="x-description text-wrap-new-lines">
-       <div> {{ explode('&$;', $lineitem->lineitem_description ?? '&$;')[1] }} </div>
-       
+       <div> {{ explode('&$;', $lineitem->lineitem_description ?? ' &$; ')[0] }} </div>
+       <div>{{ $lineitem->lineitem_description }} </div>
+       <div> {{ explode('&$;', $lineitem->lineitem_description ?? ' &$; ')[2] }} </div>
     </td>
     <!--quantity-->
     @if($lineitem->lineitem_type == 'plain')
