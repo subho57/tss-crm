78aa67f4d65b305261467e00b4cf64fd6a57b5d6
myusername <sajal1943@gmail.com>
2021-11-28 14:35:54 +0530

    #bugs fix

--- application/resources/views/pages/bill/components/elements/line-plain.blade.php
+++ application/resources/views/pages/bill/components/elements/line-plain.blade.php
@@ -40,7 +40,7 @@
     <td class="form-group x-unit bill_col_unit">
         <!-- job complexity -->
         <div class="input-group input-group-sm ">
-            <span class="input-group-addon" id="fx-line-item-min">Job <small>Complexity</small></span>
+            <span class="input-group-addon" id="fx-line-item-min">Complexity</span>
             <select class="js_item_job" aria-labelledby="bd-versions" >
                 <option class="dropdown-item" >Default</option>
                 <option class="dropdown-item" >2</option>
