75d1c9efa3943d558b1c9343753b851d33481baf
myusername <sajal1943@gmail.com>
2021-11-28 23:44:19 +0530

    bugs fix

--- application/resources/views/pages/bill/components/elements/lineitems.blade.php
+++ application/resources/views/pages/bill/components/elements/lineitems.blade.php
@@ -2,9 +2,9 @@
 <tr>
     <!--description-->
     <td class="x-description text-wrap-new-lines">
-       <div> {{explode('&$;', $lineitem->lineitem_description ?? '--')[0] }}
-       Material - {{ explode('&$;', $lineitem->lineitem_description)[1] ?? ''}}
-        {{explode('&$;', $lineitem->lineitem_description)[2] ?? ''}}
+       <div> {{explode('&$;', $lineitem->lineitem_description ?? '&$;&$;&$;1')[0] }}
+       {{ explode('&$;', $lineitem->lineitem_description ?? '&$;&$;&$;1')[1] == ''?'' : 'Material: ' . explode('&$;', $lineitem->lineitem_description ?? '&$;&$;&$;1')[1]}}
+       {{ explode('&$;', $lineitem->lineitem_description ?? '&$;&$;&$;1')[2] == ''?'' : 'Description: ' . explode('&$;', $lineitem->lineitem_description ?? '&$;&$;&$;1')[2]}}
         </div>     
     </td>
     <!--quantity-->
@@ -21,7 +21,12 @@
     </td>
     @endif
     <!--unit price-->
-    <td class="x-unit">{{ (isset($lineitem->lineitem_unit) && $lineitem->lineitem_unit == '0x0' && isset($lineitem->lineitem_total) && $lineitem->lineitem_total > 0) ? ' ' : $lineitem->lineitem_unit }}</td>
+    <td class="x-unit">
+    {{ (isset($lineitem->lineitem_unit) && $lineitem->lineitem_unit == '0x0' && isset($lineitem->lineitem_total) && $lineitem->lineitem_total > 0) ? ' ' : 'Height: ' . explode('x', $lineitem->lineitem_unit ?? '0x0')[0]  . 'mm'}}
+    {{ (isset($lineitem->lineitem_unit) && $lineitem->lineitem_unit == '0x0' && isset($lineitem->lineitem_total) && $lineitem->lineitem_total > 0) ? ' ' : 'Width: ' . explode('x', $lineitem->lineitem_unit ?? '0x0')[1]  . 'mm'}}    
+    Complexity: {{ (explode('&$;',$lineitem->lineitem_description ?? '&$;&$;&$;1')[3]/10-0.1)*100 }}%
+
+    </td>
     <!--rate-->
     <td class="x-rate">{{ runtimeNumberFormat($lineitem->lineitem_rate) }}</td>
     <!--tax-->
